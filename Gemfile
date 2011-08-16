61d79fe9ad97e7aa0c0b42f1c428a04583de3bcd
Francis Vo <firefreakfrancis@gmail.com>
2011-08-16 08:43:20 -0700

    Created working links on pages

--- Gemfile
+++ Gemfile
@@ -6,8 +6,10 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 
 group :development do
 	gem 'rspec-rails', '2.0.0.beta.18'
+  gem 'webrat', '0.7.1'
 end
 
 group :test do
 	gem 'rspec-rails', '2.0.0.beta.18'
+  gem 'webrat', '0.7.1'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -58,4 +58,21 @@ describe PagesController do
     end
   end
 
+  describe "GET 'Help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => "#{@base_title} | Help")
+    end
+
+    it "should have a non-blank body" do
+      get 'help'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+  end
+
 end
