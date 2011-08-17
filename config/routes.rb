47584c6c72e5941384507f2cc43e2d3a7743e985
Francis Vo <firefreakfrancis@gmail.com>
2011-08-17 06:38:24 -0700

    Added controller user

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,14 @@ SampleApp::Application.routes.draw do
   root :to => "pages#home"
 
   match '/contact', :to => 'pages#contact'
-
   match '/about', :to => 'pages#about'
-
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
 
+  get "users/new"
   get "pages/home"
-
   get "pages/contact"
-
   get "pages/about"
-
   get "pages/help"
 
   # The priority is based upon order of creation:
