efa4c0061746aafd67cb9cdb068d013a3acfbbaf
Francis Vo <firefreakfrancis@gmail.com>
2011-08-20 15:21:35 -0700

    added password encryption

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -93,4 +93,50 @@ describe User do
 
   end
 
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password attribute" do
+      @user.encrypted_password.should_not be_blank
+    end
+
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
+
+    describe "has_password? method" do
+      it "should exist" do
+        @user.should respond_to(:has_password?)
+      end
+      it "should return true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      it "should return false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+
+    describe "authenticat method" do
+      it "should exist" do
+        User.should respond_to(:authenticate)
+      end
+      it "should return nill on email/password mismatch" do
+        User.authenticate(@attr[:email],"wrongpass").should be_nil
+      end
+      it "should return nil for an email address with no user" do
+        User.authenticate("invalid@invalid.com", @attr[:password]).should be_nil
+      end
+      it "should return the user on email/password match" do
+        User.authenticate(@attr[:email],@attr[:password]).should == @user
+      end
+    end
+
+  end
+
 end
